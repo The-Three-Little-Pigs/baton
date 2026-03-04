6a2b3196c91363206137f73be2e88e1ad6d2ccd4
glory <thfqls7828@gmail.com>
2026-03-04 13:56:32 +0900

    feat: Post 엔티티 모델과 상품 상세 페이지 UI 컴포넌트를 추가하고, 플랫폼 플러그인 등록을 업데이트합니다.

--- lib/views/product_detail/widgets/other_product.dart
+++ lib/views/product_detail/widgets/other_product.dart
@@ -2,40 +2,34 @@ import 'package:baton/views/widgets/product_item.dart';
 import 'package:flutter/material.dart';
 
 class OtherProduct extends StatelessWidget {
-  const OtherProduct({super.key, required this.postId});
-
-  final String postId;
+  const OtherProduct({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 900,
-      width: double.infinity,
-      child: Column(
-        spacing: 20,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            "비슷한 상품들",
-            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 20,
+      children: [
+        Text(
+          "비슷한 상품들",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        GridView.builder(
+          padding: EdgeInsets.zero,
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 0.7,
+            mainAxisSpacing: 20,
+            crossAxisSpacing: 20,
           ),
-          Expanded(
-            child: GridView.builder(
-              physics: const NeverScrollableScrollPhysics(),
-              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                childAspectRatio: 0.7,
-                mainAxisSpacing: 20,
-                crossAxisSpacing: 20,
-              ),
-              itemCount: 6,
-              itemBuilder: (context, index) {
-                return ProductItem(postId: postId);
-              },
-            ),
-          ),
-        ],
-      ),
+          itemCount: 6,
+          itemBuilder: (context, index) {
+            return ProductItem(postId: "1");
+          },
+        ),
+      ],
     );
   }
 }
